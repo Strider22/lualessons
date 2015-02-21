762e48185a7fc1f7fda3f9d0250222c3b65ad6bd
Mitchel Soltys <Strider22@users.noreply.github.com>
2015-02-21 14:30:28 -0500

    Quick change to confirm pushes to github

--- personClass.lua
+++ personClass.lua
@@ -1,6 +1,7 @@
 -- http://www.tutorialspoint.com/lua/lua_modules.htm
 -- Module name and its file name should be the same.
 -- It is a best practice to return modules for require function
+-- Quick test change to verify update to github
 
 -- make member variables local
 local _name, _age
